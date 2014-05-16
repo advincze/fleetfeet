6e0099d56421108c62c901e2d6914cea39991907
Naber, Matthias <matthias.naber@asideas.de>
2014-05-16 16:45:32 +0200

    store keywords in profile

--- js/background.js
+++ js/background.js
@@ -1,6 +1,7 @@
 console.log("-.-");
 
-var Storage = function () {};
+var Storage = function () {
+};
 
 Storage.prototype.setObject = function (key, value) {
     localStorage.setItem(key, JSON.stringify(value));
@@ -25,20 +26,21 @@ if (null == storage.getObject('urls')) {
 if (null == storage.getObject('domains')) {
     storage.setObject('domains', {});
 }
+if (null == storage.getObject('profile')) {
+    storage.setObject('profile', {
+        samples: 0
+    });
+}
 
 categories = {
     shopping: ["zalando.de", "otto.de", "notebooksbilliger", "cyberport.de", "amazon.de", "amazon.com", "ebay.de", "ebay.com", "idealo.de", "guenstiger.de", "billiger.de"],
     entertainment: ["prosieben.de", "sat1.de", "kabeleins.de", "t-online.de", "maxdome.de", "watchever.de"],
     social: ["twitter.de", "twitter.com", "facebook.de", "facebook.com", "myspace.de", "linkedin.com", "pinterest.com", "tumblr.com", "instagram.com"],
-    news: ["welt.de", "spiegel.de", "faz.net", "bild.de", "n24.de", "sz.de", "zeit.de", "news.google.de"],
+    news: ["welt.de", "spiegel.de", "faz.net", "bild.de", "n24.de", "sz.de", "zeit.de", "news.google.de", "focus.de", "heise.de"],
     research: ["wikipedia.org", "wikipedia.de", "gutefrage.net", "helpster.de", "wer-weiß-was.de", "answers.yahoo.com"],
     sports: ["formel1.de", "bundesliga.de", "kicker.de", "spox.de", "sport1.de", "de.eurosport.yahoo.com", "11freunde.de", "sportschau.de"],
     leisure: ["fressnapf.de", "zooplus.de", "pokerstars.eu", "fulltiltpoker.eu", "tvmovie", "tvspielfilm", "4players.de", "yelp.de", "mcfit.com" ],
     travel: ["hotel.de", "ab-in-den-urlaub.de", "weg.de", "holidaycheck.de", "expedia.de", "tripadvisor.de"]
-};
-
-var funcA = function (details) {
-
 };
 
 var funcB = function (details) {
@@ -76,29 +78,33 @@ var funcB = function (details) {
     }
 
     var requests = storage.getObject('requests');
-    requests[details.requestId] = details;
-    storage.setObject('requests', requests);
+//    requests[details.requestId] = details; // temporary deactivated
+//    storage.setObject('requests', requests);
 
     // categorize
 
     var url = details.url;
-    var urls = storage.getObject("urls");
-    if (typeof urls[url] != "undefined") {
-        urls[url]++;
-    } else {
-        urls[url] = 1;
-    }
-    storage.setObject("urls", urls);
+//    var urls = storage.getObject("urls");
+//    if (typeof urls[url] != "undefined") {
+//        urls[url]++;
+//    } else {
+//        urls[url] = 1;
+//    }
+//    storage.setObject("urls", urls);
+
     var domain = url.match(/(\w+\.\w+)\//)[1];
     var headers = details.requestHeaders, blockingResponse = {};
-    var contentLength = 0;
-    for (var i = 0, l = details.responseHeaders.length; i < l; ++i) {
-        if (details.responseHeaders[i].name == "Content-Length") {
-            contentLength = details.responseHeaders[i].value;
-            break;
+
+    function getHeader(name, fallback) {
+        for (var i = 0, l = details.responseHeaders.length; i < l; ++i) {
+            if (details.responseHeaders[i].name == name) {
+                return details.responseHeaders[i].value;
+            }
         }
+        return fallback;
     }
 
+    var contentLength = getHeader("Content-Length", 0);
     /** skip this URL if size is above threshold and it is of a certain type */
     var skip = /(\.jpeg|\.jpg|\.png|\.gif|\.bmp|\.css)$/g.test(url)
         && contentLength > 200;
@@ -116,15 +122,55 @@ var funcB = function (details) {
         storage.setObject("domains", domains);
     }
 
+
+
+    if (getHeader("Content-Type", null).indexOf("text/html") === 0) {
+
+        function analyseKeywords(keywords) {
+            var profile = storage.getObject("profile");
+
+            profile.samples++;
+
+            for (var i = 0; i < keywords.length; i++) {
+                var keyword = keywords[i].toLowerCase();
+                if (typeof profile[keyword] == "undefined") {
+                    profile[keyword] = 1;
+                } else {
+                    profile[keyword]++;
+                }
+            }
+            storage.setObject("profile", profile);
+        }
+
+        $.get(url, function (data) {
+            var match = /meta\s+name="keywords"\s+content="(.*)"/gm.exec(data);
+            function htmlDecode(value){
+                return $('<div/>').html(value).text();
+            }
+            if (null != match && match.length > 0) {
+                var keywords = htmlDecode(match[1]).split(/[,;]+/);
+                if (typeof keywords != "undefined" && keywords.length > 0) {
+                    analyseKeywords(keywords);
+                }
+            }
+        });
+    }
+
     blockingResponse.requestHeaders = headers;
     return blockingResponse;
 };
-/** send */
-chrome.webRequest.onBeforeSendHeaders.addListener(funcA,
-    {urls: [ "<all_urls>" ]},
-    ['requestHeaders', 'blocking']);
-
+///** send */
+//chrome.webRequest.onBeforeSendHeaders.addListener(funcA,
+//    {urls: [ "<all_urls>" ]},
+//    ['requestHeaders', 'blocking']);
+//
 /** receive */
 chrome.webRequest.onHeadersReceived.addListener(funcB,
     {urls: [ "<all_urls>" ]},
     ['responseHeaders', 'blocking']);
+
+/** completed */
+//chrome.webRequest.onCompleted.addListener(function (details) {
+//        console.log(details);
+//    },
+//    {urls: [ "<all_urls>" ]});
--- popup.js
+++ popup.js
@@ -1,1 +1,2 @@
 console.log("popup");
+//alert('foo');
