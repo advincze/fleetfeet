6cbb6bfa1d80fe4d45d9ee2cadc8a38358976103
Naber, Matthias <matthias.naber@asideas.de>
2014-05-16 13:44:50 +0200

    skipping large images

--- js/background.js
+++ js/background.js
@@ -9,24 +9,25 @@ categories = {
     entertainment: ["prosieben.de", "sat1.de", "kabeleins.de", "t-online.de", "maxdome.de", "watchever.de"],
     social: ["twitter.de", "twitter.com", "facebook.de", "facebook.com", "myspace.de", "linkedin.com", "pinterest.com", "tumblr.com", "instagram.com"],
     news: ["welt.de", "spiegel.de", "faz.net", "bild.de", "n24.de", "sz.de", "zeit.de", "news.google.de"],
-    research: ["wikipedia.org", "gutefrage.net", "helpster.de", "wer-weiß-was.de", "answers.yahoo.com"],
+    research: ["wikipedia.org", "wikipedia.de", "gutefrage.net", "helpster.de", "wer-weiß-was.de", "answers.yahoo.com"],
     sports: ["formel1.de", "bundesliga.de", "kicker.de", "spox.de", "sport1.de", "de.eurosport.yahoo.com", "11freunde.de", "sportschau.de"],
     leisure: ["fressnapf.de", "zooplus.de", "pokerstars.eu", "fulltiltpoker.eu", "tvmovie", "tvspielfilm", "4players.de", "yelp.de", "mcfit.com" ],
     travel: ["hotel.de", "ab-in-den-urlaub.de", "weg.de", "holidaycheck.de", "expedia.de", "tripadvisor.de"]
 };
 
+var funcA = function (details) {
 
-chrome.webRequest.onHeadersReceived.addListener(function (details) {
+};
 
-//        console.log("Received", details);
-        requests[details.requestId].received = details;
+var funcB = function (details) {
 
-    },
-    {urls: [ "<all_urls>" ]},
-    ['responseHeaders', 'blocking']);
-
-
-var beforeSendHeadersFunc = function (details) {
+    function registerPreference(category) {
+        if (typeof preferences[category] === "undefined") {
+            preferences[category] = 1;
+        } else {
+            preferences[category]++;
+        }
+    }
 
     function categorizeDomain(domain) {
 
@@ -35,16 +36,25 @@ var beforeSendHeadersFunc = function (details) {
                 var urls = categories[category];
                 for (var i = 0; i < urls.length; i++) {
                     if (domain === urls[i]) {
-                        if (typeof preferences[category] === "undefined") {
-                            preferences[category] = 1;
-                        } else {
-                            preferences[category]++;
-                        }
+                        registerPreference(category);
                     }
                 }
             }
         }
     }
+
+//    function categorizeSearch(domain, url) {
+//        switch (domain) {
+//            case "google.de":
+//                registerPreference(/q=(.*)(&|$)]/.exec(url)[1]);
+//                break;
+//            case "amazon.de":
+//                break;
+//
+//            case "twitter.com":
+//                break;
+//        }
+//    }
 
     var headers = details.requestHeaders, blockingResponse = {};
 
@@ -52,29 +62,43 @@ var beforeSendHeadersFunc = function (details) {
 
     // categorize
 
-    if (typeof urls[details.url] != "undefined") {
-        urls[details.url]++;
+    var url = details.url;
+    if (typeof urls[url] != "undefined") {
+        urls[url]++;
     } else {
-        urls[details.url] = 1;
+        urls[url] = 1;
     }
-    var domain = details.url.match(/(\w+\.\w+)\//)[1];
+    var domain = url.match(/(\w+\.\w+)\//)[1];
 
-    if (typeof domains[domain] != "undefined") {
-        categorizeDomain(domain);
-        domains[domain]++;
-    } else {
-        domains[domain] = 1;
-    }
-    for (var i = 0, l = headers.length; i < l; ++i) {
-        if (headers[i].name == 'User-Agent') {
-//            console.log("Send", details);
-            //console.log(headers[i].value);
+    var contentLength = 0;
+    for (var i = 0, l = details.responseHeaders.length; i < l; ++i) {
+        if (details.responseHeaders[i].name == "Content-Length") {
             break;
         }
     }
+
+    var skip = /(\.gif|\.png|\.gif|\.bmp)$/g.test(url)
+        && contentLength > 1000;
+
+    if (!skip) {
+
+        if (typeof domains[domain] != "undefined") {
+            categorizeDomain(domain);
+            domains[domain]++;
+        } else {
+            domains[domain] = 1;
+        }
+    }
+
     blockingResponse.requestHeaders = headers;
     return blockingResponse;
 };
-chrome.webRequest.onBeforeSendHeaders.addListener(beforeSendHeadersFunc,
+/** send */
+chrome.webRequest.onBeforeSendHeaders.addListener(funcA,
     {urls: [ "<all_urls>" ]},
     ['requestHeaders', 'blocking']);
+
+/** receive */
+chrome.webRequest.onHeadersReceived.addListener(funcB,
+    {urls: [ "<all_urls>" ]},
+    ['responseHeaders', 'blocking']);
