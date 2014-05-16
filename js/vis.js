832072d47d7433ce547603b108cd9f8bc6e7a86e
Viktor Mucha <vmucha@me.com>
2014-05-16 13:44:05 +0200

    bars for categories

--- js/vis.js
+++ js/vis.js
@@ -6,24 +6,33 @@ function initBars(data) {
 	var paddingY = 20;
 	var barHeight = 20;
 	var labelWidth = 80;
-	var labelPadding = 5;
-	var barPadding = 5;
+	var labelPadding = 20;
+	var barPadding = 10;
+	var maxValue = 0;
 //	var w = window.innerWidth-16;
 
 var w = window.innerWidth-16;
 var h = window.innerHeight-16;
+
+
+maxValue = d3.max(data,function(d) {
+	return Math.log(d.value);
+});
+
 //var h = nToShow*(barHeight+barPadding)+100;
 var scaleX = d3.scale.linear();
 	//scaleX.domain([0,maxes[keys[0]]]);
-	scaleX.domain([0,10]);
+	scaleX.domain([0,maxValue]);
 	scaleX.range([0,w-paddingX-1-labelWidth-labelPadding]);
 var colorScale = d3.scale.linear();
-	colorScale.domain([0,10]);
+	colorScale.domain([0,maxValue]);
 	colorScale.range([0,255]);
 var xAxis = d3.svg.axis()
 	.scale(scaleX)
 	.orient("bottom")
 	.ticks(10);
+
+
 var svg = d3.select("body")
 		.append("svg")
 		.attr("width", w)
@@ -33,10 +42,10 @@ var svg = d3.select("body")
 	       	.enter()
 	       	.append("rect")
 	       	.attr("height",barHeight)
+	  		.attr("width",0)
 	  		.attr("y",function(d,i) {
-	       		return i*(barHeight+barPadding);
-	      	})       
-	  		.attr("width",100)
+	       		return barHeight*i+barPadding*i;
+	       	})
 			.attr("x",paddingX)
 	       	.transition()
 	      	.duration(450)
@@ -44,15 +53,31 @@ var svg = d3.select("body")
 	       		return 200*i;
 	       	})
      		.attr("width",function(d,i) {
-	       		
-	       			var sumX = 0;
-	       			
-	       			return scaleX(sumX);
-	       		
-       		});
-    
+     			return scaleX(Math.log(d.value));
+       		})
+			.attr("fill",function(d,i) {
+				return "rgb("+Math.round(colorScale(Math.log(d.value)))+",0,0)";
+	   		});
+	   	svg.selectAll("svg")
+	   		.data(data)
+	   		.enter()
+	   		.append("text")
+	   		.text(function(d,i) {
+	   			return d.name;
+	   			})
+	   		.attr("y",function(d,i) {
+	   			return barHeight*i+barPadding*i+barHeight/2;
+	   		})
+	   		.attr("x",paddingX)
+	   		.transition()
+	   		.duration(450)
+	   		.delay(function(d,i) {
+	   			return 200*i;
+	   		})
+	   		.attr("x",function(d,i) {
+	   			return scaleX(Math.log(d.value))+labelPadding;
+	   		})
+	   		;
 }
-
-
 //initBars([{news:5},{entertainment:10}]);
 
