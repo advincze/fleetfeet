ba9b02eb5ec015bb17f7d45d04abe34dec14b0f2
Naber, Matthias <matthias.naber@asideas.de>
2014-05-16 12:42:43 +0200

    transforming preferences into barData-compatible version

--- js/page.js
+++ js/page.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
     console.log("test");
+
+    $("#refresh").click(function () {
+
+        var barData = [];
+
+        for (var preference in preferences) {
+            if (preferences.hasOwnProperty(preference)) {
+                barData.push({
+                    value: preferences[preference],
+                    name: preference
+                });
+            }
+        }
+
+        initBars(barData)
+
+    })
 });
